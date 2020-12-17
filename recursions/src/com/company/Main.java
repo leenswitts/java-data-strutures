ba41db840eeeb44ff5184227c69174473b9bd114
witts <leenswitts@gmail.com>
2020-12-18 04:48:37 +0530

    recursions

---
+++ recursions/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    System.out.println(iterativeFactorial(6));
+    System.out.println(recursiveFactorial(6));
+
+    }
+
+    // iterative factorial
+    public  static int iterativeFactorial(int num){
+        if(num == 0){
+            return 1;
+        }
+        int factorial = 1;
+        for (int i = 1; i <= num; i++){
+            factorial *= i;
+        }
+        return factorial;
+
+    }
+    // recursive factorial
+    //n! = n * (n -1)!
+    public static  int recursiveFactorial(int num){
+        if(num == 0){
+            return 1;
+        }
+        return  num * recursiveFactorial(num -1);
+    }
+
+}
