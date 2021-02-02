8cc28a72378054470d6be9259ce25db09c2346cb
witts <leenswitts@gmail.com>
2021-02-02 23:19:00 +0530

    circular queues

--- queues/circularQueues/src/com/company/Main.java
+++ queues/circularQueues/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Employee leens = new Employee("leens", "witts", 23);
+        Employee amos = new Employee("Amos", "Remijo", 23);
+        Employee kevin = new Employee("Kevin", "Naj", 23);
+
     }
 }
